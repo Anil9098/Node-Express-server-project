d909cb6682edf042126e813aeaf318980a16a0c3
root <root@ip-172-31-0-142.ap-south-1.compute.internal>
2024-12-19 04:50:15 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Root Route - Ensure it's added before or after your middleware
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js API!');
+});
+
+
+
 // Custom Middleware
 
 app.use((req, res, next) => {
@@ -39,3 +48,8 @@ app.use('/messages', routes.message);
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
+
+
+
+
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import session from './session';
 import user from './user';
 import message from './message';
 
+
 export default {
   session,
   user,
